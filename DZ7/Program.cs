7960d842c7d892372b9c53ef8e755948d8e098ce
Dmitriy Zykin <zhuk-spb@yandex.ru>
2023-01-11 01:28:30 +0400

    Added tasks 2,3 for DZ7

--- DZ7/Program.cs
+++ DZ7/Program.cs
@@ -30,14 +30,14 @@ Console.Clear();
 System.Console.WriteLine("Введите размер массива M х N");
 int m = inputNumber("M = ");
 int n = inputNumber("N = ");
-double [,] array = new double[m,n];
+int [,] array = new int[m,n];
 
 Random rand = new Random();
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i,j] = Math.Round (rand.Next(-9,10) + rand.NextDouble(),1);
+        array[i,j] = Math.Round (rand.Next(-9,10) + rand.Nextint(),1);
         System.Console.Write(array[i,j]+" ");
     }
     System.Console.WriteLine();
@@ -45,8 +45,9 @@ for (int i = 0; i < array.GetLength(0); i++)
 
 // ***************************************************************************************
 
-// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
-// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Задача 50. Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве,и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
 // Например, задан массив:
 
 // 1 4 7 2
@@ -57,13 +58,135 @@ for (int i = 0; i < array.GetLength(0); i++)
 
 // Решение:
 
+int inputNumber(string str)
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Не удалось распознать число, попробуйте ещё раз.");
+    }
+    return number;
+}
+
+int[,] fillArray(int m, int n)
+{
+    int [,] array = new int[m,n];
+
+Random rand = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = rand.Next(-9,10);
+        System.Console.Write(array[i,j]+"\t");
+    }
+    System.Console.WriteLine();
+}
+return array;
+} 
+
+string elementCheckArray(int[,] array, int i, int j)
+{
+    string result = string.Empty;
+if (i < array.GetLength(0) && j < array.GetLength(1)) 
+{
+result = Convert.ToString (array[i,j]);
+}
+else
+{
+    result = "Такого числа в массиве нет!";
+}
+return result;
+}
+
+int m = inputNumber("Введите количество строк М = ");
+int n = inputNumber("Введите количество строк N = ");
+int[,] finalArray = fillArray(m,n);
+int i = inputNumber("Введите номер строки i = "); 
+int j= inputNumber("Введите номер строки j = ");
+System.Console.WriteLine(elementCheckArray(finalArray,i,j));
+
+
+
 // ***************************************************************************************
 
 // Задача 52. Задайте двумерный массив из целых чисел. 
 // Найдите среднее арифметическое элементов в каждом столбце.
+
 
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Решение:
+
+int inputNumber(string str)
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Не удалось распознать число, попробуйте ещё раз.");
+    }
+    return number;
+}
+
+int[,] fillArray(int m, int n)
+{
+    Console.Clear();
+    int[,] array = new int[m, n];
+
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(-9, 10);
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+    return array;
+}
+
+void average(int[,] array, out double[] avrgNumbers)
+{
+    avrgNumbers = new double[array.GetLength(1)];
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            avrgNumbers[j] += array[i, j];
+        }
+        avrgNumbers[j] = avrgNumbers[j] / (array.GetLength(0));
+    }
+
+}
+
+int m = inputNumber("Введите количество строк М = ");
+int n = inputNumber("Введите количество строк N = ");
+int[,] finalArray = fillArray(m, n);
+average(finalArray, out double[] avrgNumbers);
+System.Console.WriteLine($"Среднее арифметическое каждого столбца {string.Join("\t", avrgNumbers)}");
+
+
+
+//**********************************************************************************
