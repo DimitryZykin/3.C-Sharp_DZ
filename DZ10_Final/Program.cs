e613df98f796f03c0e40dd0e5e8f668733870d07
Dmitriy Zykin <zhuk-spb@yandex.ru>
2023-01-30 20:48:28 +0400

    Added DZ10 first iteration

---
+++ DZ10_Final/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+
+// Searching <= 3 chars elements method
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
